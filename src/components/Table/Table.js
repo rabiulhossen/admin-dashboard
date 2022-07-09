2bca33862deeb5509cb5fe340a305bab32e79b47
rabiulhossen <rhsimon69@gmail.com>
2022-07-09 22:43:05 +0600

    chart and table without styles

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -7,38 +7,59 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import './Table.css'
 
 const useStyles = makeStyles({
   table: {
-    minWidth: 650,
+    minWidth: 550,
   },
 });
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+function createData(name,TrackingId, date,Status) {
+  return { name,TrackingId, date,Status };
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Frozen yoghurt', 159, 6.0,'Approved'),
+  createData('Ice cream sandwich', 237, 9.0,'Approved'),
+  createData('Eclair', 262, 16.0, 'Pending'),
+  createData('Cupcake', 305, 3.7, 'Pending'),
+  createData('Gingerbread', 356, 16.0,'not provide'),
 ];
 
 export default function BasicTable() {
   const classes = useStyles();
+const makeStyles =(Status)=>{
+if(Status === 'Approved'){
+  return{
+    background:'green',
+    color:'red'
+  }
+}
+else if(Status === 'Pending'){
+  return{
+    background:'yellow',
+    color:'pink'
+  }
+}
+else{
+  return{
+    background:'orange',
+    color:'black'
+  }
+}
+}
 
   return (
     <TableContainer component={Paper}>
       <Table className={classes.table} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell align="left">Tracking Id</TableCell>
+            <TableCell align="left">Status</TableCell>
+            <TableCell align="left">Date</TableCell>
+            
           </TableRow>
         </TableHead>
         <TableBody>
@@ -47,10 +68,11 @@ export default function BasicTable() {
               <TableCell component="th" scope="row">
                 {row.name}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="left">{row.TrackingId}</TableCell>
+              <TableCell align="left"><span style={makeStyles(row.Status)} className='status' >
+                {row.Status}</span></TableCell>
+              <TableCell align="left" >{row.date}</TableCell>
+             
             </TableRow>
           ))}
         </TableBody>

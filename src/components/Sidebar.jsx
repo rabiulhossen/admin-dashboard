6aa5254fbf5dada27f81e2e2454fc466e9cea20b
Rabiul-Hossen <rhsimon69@gmail.com>
2023-03-22 23:18:32 +0600

    hsib data added

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,21 +1,36 @@
 import React from 'react';
-import '../components/css/Sidebar.css'
+import '../components/css/Sidebar.css';
+
+
+import { SidebarData } from '../assets/Data/Data';
 
 function Sidebar() {
-  return (
-    <div className='sidebar'>
-     {/* // logo  */}
-<div className="logo">
-     <img src="https://www.freepnglogos.com/uploads/shopping-cart-png/shopping-cart-donations-shopping-carts-forms-membershipworks-21.png" alt=""  />
-     <span>Sh
+   return (
+      <div className="sidebar">
+         {/* // logo  */}
+         <div className="logo">
+            <img
+               src="https://www.freepnglogos.com/uploads/shopping-cart-png/shopping-cart-donations-shopping-carts-forms-membershipworks-21.png"
+               alt=""
+            />
+            <span>
+               Sh
+               <span>o</span>ps
+            </span>
+         </div>
+         <div className="menu">
+          {
+SidebarData.map((item)=>{
+     <div className="menuItem" key={item.id}>
+          <item.icon/>
+          <span>{item.heading}</span>
+     </div>
 
-          <span>o</span>ps
-     </span>
-</div>
-
-
-    </div>
-  )
+})
+          }
+         </div>
+      </div>
+   );
 }
 
-export default Sidebar
+export default Sidebar;
